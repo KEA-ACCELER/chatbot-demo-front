6b0198559aa27102e4b077fc667b79b198289a80
masibasi <jimin_lee98@naver.com>
2023-04-22 22:31:57 +0900

    css change

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React, { useState } from "react";
 import UsernameForm from "./components/UsernameForm";
 import ChatRoom from "./components/ChatRoom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 function App() {
-  const [username, setUsername] = useState("");
+    const [username, setUsername] = useState("");
 
-  function handleUsernameSubmit(newUsername) {
-    setUsername(newUsername);
-  }
+    function handleUsernameSubmit(newUsername) {
+        setUsername(newUsername);
+    }
 
-  if (username) {
-    return <ChatRoom username={username} />;
-  } else {
-    return <UsernameForm onSubmit={handleUsernameSubmit} />;
-  }
+    return (
+        <div className="Background">
+            <div className="App">
+                {username === "" ? (
+                    <UsernameForm onSubmit={handleUsernameSubmit} />
+                ) : (
+                    <ChatRoom username={username} />
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/UsernameForm.js
+++ src/components/UsernameForm.js
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
-import "../App.css";
+
+import Button from "react-bootstrap/Button";
 
 function UsernameForm(props) {
-  const [username, setUsername] = useState("");
+    const [username, setUsername] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    props.onSubmit(username);
-  }
+    function handleSubmit(event) {
+        event.preventDefault();
+        props.onSubmit(username);
+    }
 
-  function handleChange(event) {
-    setUsername(event.target.value);
-  }
+    function handleChange(event) {
+        setUsername(event.target.value);
+    }
 
-  return (
-    <div id="username-page">
-      <div className="username-page-container">
-        <h1 className="title">Type your username</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <input type="text" id="name" placeholder="Username" value={username} onChange={handleChange} autoComplete="off" className="form-control" />
-          </div>
-          <div className="form-group">
-            <button type="submit" className="accent">
-              Start Chatting
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className="username-page">
+            <h1 className="title">Type your username</h1>
+
+            <input
+                type="text"
+                id="name"
+                placeholder="Username"
+                value={username}
+                onChange={handleChange}
+                autoComplete="off"
+                className="username-input"
+            />
+
+            <Button variant="outline-warning" onClick={handleSubmit}>
+                Start Chatting
+            </Button>
+        </div>
+    );
 }
 
 export default UsernameForm;
